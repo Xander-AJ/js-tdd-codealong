af33dfc2030b1ed3e5f697999fca132827814445
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-05-02 07:50:03 +0300

    complete lab

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
 // Your code here
+// utils.js
+
+/**
+ * Calculate the age of a person based on their birth year.
+ * @param {number} birthYear The year the person was born.
+ * @returns {number} The age of the person.
+ */
+export function currentAgeForBirthYear(birthYear) {
+    const currentYear = new Date().getFullYear();
+    const age = currentYear - birthYear;
+    return age;
+  }
+  
